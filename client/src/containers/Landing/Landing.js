a3b3b2073bb03ff5dd842789268a6385a50c8c03
Jonathan Plata <jonathan_plata@ymail.com>
2018-04-01 19:16:03 -0700

    landing body fix

--- client/src/containers/Landing/Landing.js
+++ client/src/containers/Landing/Landing.js
@@ -10,26 +10,30 @@ class Landing extends Component {
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.goBack = this.goBack.bind(this);
   }
 
   handleClick(event) {
     var name = event.target.name;
-    console.log(name);
     this.setState({action: name});
+  }
+
+  handleChange() {
+    // to do...
+  }
+
+  handleSubmit() {
+    // to do
+  }
+
+  goBack() {
+    this.setState({action: 'standard'});    
   }
 
   render() {
     const action = this.state.action;
-
-    switch(action) {
-      case 'standard':
-        console.log('standard');
-        break;
-
-      default:
-        console.log('def');
-        break;
-    }
 
   	return(
       <div>
@@ -37,17 +41,35 @@ class Landing extends Component {
           <h3>Spotify DJ</h3>
         </div>
         <div className="Landing-Body">
-          <div id="toHide">
-            <button name="create" className="btn" onClick={this.handleClick}>Create</button>
-            <button name="join" className="btn" onClick={this.handleClick}>Join</button>
-          </div>
-          <div>
-            {this.state.create ?
-              <p>sup sup</p>
-              : 
-              <p>no no nno</p>
-            }
-          </div>
+          {action === 'standard' &&
+            <div>
+              <button name="create" className="btn" onClick={this.handleClick}>Create</button>
+              <button name="join" className="btn" onClick={this.handleClick}>Join</button>
+            </div>
+          }
+          {action === 'create' &&
+            <div>
+              <button className="Back-Btn" onClick={this.goBack}>x</button>
+              <form onSubmit={this.handleSubmit}>
+                <input 
+                  type="text"
+                  name="event"
+                  onChange={this.handleChange}
+                />
+                <input 
+                  type="text"
+                  name="host"
+                  onChange={this.handleChange}                  
+                />
+              </form>
+            </div>
+          }
+          {action === 'join' &&
+            <div>
+              <button className="Back-Btn" onClick={this.goBack}>X</button>
+              <p>Join</p>
+            </div>
+          }
         </div>
       </div>
   	);
